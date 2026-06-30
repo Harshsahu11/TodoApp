a4e0f257533fad5999e9f5f9175a41fd76717ffc
Harsh Sahu <harshsahu1109@gmail.com>
2026-06-30 12:02:31 +0530

    Task contoller,service and other

--- src/main/java/com/detrox/TodoApp/controller/TaskController.java
+++ src/main/java/com/detrox/TodoApp/controller/TaskController.java
@@ -1,45 +1,38 @@
 package com.detrox.TodoApp.controller;
 
-import com.detrox.TodoApp.model.Task;
 import com.detrox.TodoApp.services.TaskService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController
-@RequestMapping("/")
+@Controller
 @AllArgsConstructor
+@RequestMapping("/")
 public class TaskController {
 
-    @Autowired
     private final TaskService taskService;
 
     @GetMapping
     public String getTasks(Model model) {
-        List<Task> tasks = taskService.getAllTasks();
-        model.addAttribute("tasks",tasks);
+        model.addAttribute("tasks", taskService.getAllTasks());
         return "tasks";
-
     }
 
     @PostMapping
-    public String createTask(@RequestParam String title){
+    public String createTask(@RequestParam String title) {
         taskService.addTask(title);
         return "redirect:/";
     }
 
     @GetMapping("/{id}/delete")
-    public String deleteTask(@RequestParam Long id){
+    public String deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
         return "redirect:/";
     }
 
     @GetMapping("/{id}/toggle")
-    public String toggleTask(@RequestParam Long id){
+    public String toggleTask(@PathVariable Long id) {
         taskService.toggleTask(id);
         return "redirect:/";
     }
